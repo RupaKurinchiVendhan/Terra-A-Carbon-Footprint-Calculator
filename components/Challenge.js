53d809281a68f2e689342d8285e2ac700ec6cbbd
emleee <emlee2002@gmail.com>
2021-04-17 23:45:01 -0700

    idk tbh

--- components/Challenge.js
+++ components/Challenge.js
@@ -8,78 +8,7 @@ import * as Animatable from 'react-native-animatable';
 import Collapsible from 'react-native-collapsible';
 import Accordion from 'react-native-collapsible/Accordion';
 
-// var task3 = new Task(
-//     'Eat locally-grown foods and produce.',
-//     "Reduce the emissions from transport while supporting your local farmers and businesses!",
-//     './assets/produce.png',
-//     "Consumption/Waste");
-// var task4 = new Task(
-//     'Use your food waste as compost.',
-//     "Composting helps reduce food waste, decreasing the fossil fuel consumption from waste transportation and the emission of greenhouse gases from decomposition in a landfill.",
-//     './assets/compost.png',
-//     'Consumption/Waste');
-// var task5 = new Task(
-//     'Buy a reusable water bottle.',
-//     "The production of single-use plastic bottles is reported to release 2.5 million tons of carbon dioxide into the atmosphere every year.",
-//     './assets/bottle.png',
-//     'Consumption/Waste');
-// var task6 = new Task(
-//     "Donate clothes that you don't need and repurpose un-donatable ones.",
-//     "Help reduce the methane emitted from decomposition of clothes in landfills while helping someone in need or crafting a fun DIY.",
-//     './assets/clothes.png',
-//     'Consumption/Waste');
-// var task7 = new Task(
-//     'Research the companies you purchase from.',
-//     "It helps to support and buy from companies that are environmentally responsible and sustainable. Energy Star products offer a wide variety of appliances that are certified to be more energy efficient.",
-//     './assets/bulbs.png',
-//     'Consumption/Waste');
-// var task8 = new Task(
-//     'Use reusable bags when shopping.',
-//     "Reusable bags are an easy way to reduce various sources of carbon emissions that accompany he waste management process.",
-//     './assets/recycle.png',
-//     'Consumption/Waste');
-// var task9 = new Task(
-//     'Replace all light bulbs in your home with LEDs.',
-//     "LEDs waste less energy and last longer, so even though they are more expensive up front, they will be beneficial both the environment and your wallet in the long term!",
-//     './assets/bulbs.png',
-//     'Utilities');
-// var task10 = new Task(
-//     'Turn off lights and unplug electronic devices when not in use.',
-//     "These small actions can help to greatly reduce any energy waste in your home.",
-//     './assets/bulbs.png',
-//     'Utilities');
-// var task11 = new Task(
-//     'Properly insulate and seal your home.',
-//     "Reducing drafts and air leaks will prevent any unnecessary heating and cooling that could waste energy in your home.",
-//     './assets/bulbs.png',
-//     'Utilities');
-// var task12 = new Task(
-//     'Avoid unnecessary braking and acceleration when driving.',
-//     "It has been shown that smoother and calmer driving reduces fuel consumption and accompanying emissions when on the road.",
-//     './assets/walk.png',
-//     'Transportation');
-// var task13 = new Task(
-//     'Keep your car maintenance up to date.',
-//     "Taking care of your car increases fuel efficiency and reduces unnecessary emissions.",
-//     './assets/walk.png',
-//     'Transportation');
-// var task14 = new Task(
-//     'Take other modes of transportation instead of driving or try to carpool.',
-//     "These alternatives can either completely cut the harmful emissions from your trip or at least decrease the fuel consumption per person on the vehicle.",
-//     './assets/walk.png',
-//     'Transportation');
-// var task15 = new Task(
-//     'Fly economy.',
-//     "Economy is the least harmful to the environment because the fuel consumption per person transported is much lower than the business and first classes.",
-//     './assets/shipping.png',
-//     'Transportation');
-// var task16 = new Task(
-//     'Donate to a climate science organization.',
-//     'If you have the means to donate to climate change charities, consider contributing to research and raising awareness. Some high-imact, evidence-based, cost-effective organizations include The Coalition for Rainforest Nations, Clean Air Task Force, The Information Technology and Innovation Foundation, Rainforest Foundation US, Sandbag, and The Climate Emergency Fund.',
-//     './assets/donate.png',
-//     'Miscellaneous');
-
-const CONTENT = [
+var CONTENT = [
   {title: 'Eat lower on the food chain.', content: "14.5% of global greenhouse gas emissions can be attributed to the livestock industry from sources such as feed production, clearing land, and methane emissions from the animals themselves.", source: require('./assets/eat.png'), key: 1, category: "Consumption/Waste", done: False},
   {title: 'Eat on smaller plates to limit portion sizes and eliminate food waste.', content: "A study performed by Roskilde University revealed that if the plate size is reduced by just 9%, the food waste can be reduced by over 25%. In other words, by reducing the size of the plate, you ensure that you don't over feed yourself or the trash bin!", source: require('./assets/plate.png'), key: 2, category: 'Consumption/Waste', done: False},
   {title: 'Eat locally-grown foods and produce.', content: "Reduce the emissions from transport while supporting your local farmers and businesses!", source: require('./assets/produce.png'), key: 3, category: "Consumption/Waste", done: False},
@@ -98,6 +27,8 @@ const CONTENT = [
   {title: 'Donate to a climate science organization.', content: 'If you have the means to donate to climate change charities, consider contributing to research and raising awareness. Some high-imact, evidence-based, cost-effective organizations include The Coalition for Rainforest Nations, Clean Air Task Force, The Information Technology and Innovation Foundation, Rainforest Foundation US, Sandbag, and The Climate Emergency Fund.', source: require('./assets/donate.png'), key: 16, category: "Miscellaneous", done: False},
 ]
 
+const calculator = new Footprint_Calculator(CONTENT);
+
 const SELECTORS = CONTENT.map(function(x) {return {title: x.title}});
 
 const layoutAnimConfig = {
@@ -114,7 +45,6 @@ const layoutAnimConfig = {
 
 const ChallengeList = () => {
   const [data, setData] = useState(CONTENT);
-  const [done, setDone] = useState(False); //idk about this line
 
   const removeItem = (title) => {
      let arr = data.filter(function(item) {
--- components/assets/Footprint_Calculator.js
+++ components/assets/Footprint_Calculator.js
@@ -1,262 +1,270 @@
-var max_transportation = 55;
-var max_waste = 110;
-var max_utility = 30;
-// var answers = list;
-// var tiers = {};
+class Footprint_Calculator {
+    
+    constructor(list) {
+        this.max_transportation = 55;
+        this.max_waste = 110;
+        this.max_utility = 30;
+        this.answers = list;
+        this.tiers = {};
 
-// calculate scores for each category
-var transportation_score = 0;
-var waste_score = 0;
-var utility_score = 0;
-// var transportation_score = transportation(list.slice(0,3));
-// var waste_score = waste(list.slice(3,9));
-// var utility_score = utility(list.slice(9,12));
+        // calculate scores for each category
+        this.transportation_score = 0;
+        this.waste_score = 0;
+        this.utility_score = 0;
+        // this.car_transportation(list[0]);
+        // this.public_transportation(list[1]);
+        // this.flight_transportation(list[2]);
+        // calculate_waste(list.slice(3,9));
+        // calculate_utility(list.slice(9,12));
 
-// calculate results
-var total_score = transportation_score + waste_score + utility_score;
-var max_total = max_transportation + max_waste + max_utility;
-// tiers = tier(tiers);
-
-// Personal/public/flights and miles per year
-function car_transportation(car) {
-    // miles of car usage per year
-    if (car > 15000) {
-        transportation_score += 15;
-    }
-    else if (car > 10000) {
-        transportation_score += 10;
-    }
-    else if (car > 1000) {
-        transportation_score += 6;
-    }
-    else if (car > 0) {
-        transportation_score += 4;
+        // calculate results
+        this.total_score = this.transportation_score + this.waste_score + this.utility_score;
+        this.max_total = max_transportation + max_waste + max_utility;
+        this.tiers = tier(tiers);
     }
 
-    return transportation_score;
-}
-
-function public_transportation(buses) {
-    // miles of public transportation usage per year
-    if (buses > 20000) {
-        transportation_score += 20;
-    }
-    else if (buses > 15000) {
-        transportation_score += 10;
-    }
-    else if (buses > 10000) {
-        transportation_score += 6;
-    }
-    else if (buses > 1000) {
-        transportation_score += 4;
-    }
-    else if (buses > 0) {
-        transportation_score += 2;
-    }
-    return transportation_score;
-}
-
-function flight_transportation(flight) {
-    // flight distance per year
-    if (flight == 0) {
-        transportation_score += 2;
-    }
-    else if (flight == 1) {
-        transportation_score += 6;
-    }
-    else if (flight == 2) {
-        transportation_score += 20;
-    }
-    return transportation_score;
-}
-
-function get_transportation() {
-    return '%.2f'%(transportation_score / max_transportation);
-}
-
-// Eating meat, household purchases, trash cans per week, types of waste recycled
-// function waste(list) {
-//     var waste_score = 0;
-//
-//     // Meat per week
-//     if (list[0] == 7) {
-//         waste_score += 10;
-//     }
-//     else if (list[0] > 2) {
-//         waste_score += 8;
-//     }
-//     else if (list[0] > 0) {
-//         waste_score += 5;
-//     }
-//
-//     // Vegan y/n
-//     if (list[1] == 1) {
-//         waste_score += 2;
-//     }
-//     else if (list[1] == 0 && list[0] == 0) {
-//         waste_score += 4;
-//     }
-//
-//     // Diet type (prepackaged, fresh, mix)
-//     if (list[2] == 0) {
-//         waste_score += 12;
-//     }
-//     else if (list[2] == 1) {
-//         waste_score += 6;
-//     }
-//     else if (list[2] == 2) {
-//         waste_score += 2;
-//     }
-//
-//     // Household purchases per year
-//     if (list[3] > 7) {
-//         waste_score += 10;
-//     }
-//     else if (list[3] > 5) {
-//         waste_score += 8;
-//     }
-//     else if (list[3] > 3) {
-//         waste_score += 6;
-//     }
-//     else if (list[3] > 0) {
-//         waste_score += 4;
-//     }
-//     else {
-//         waste_score += 2;
-//     }
-//
-//     // Trash cans filled per week
-//     if (list[4] >= 4) {
-//         waste_score += 50;
-//     }
-//     else if (list[4] == 3) {
-//         waste_score += 40;
-//     }
-//     else if (list[4] == 2) {
-//         waste_score += 30;
-//     }
-//     else if (list[4] == 1) {
-//         waste_score += 20;
-//     }
-//     else {
-//         waste_score += 5;
-//     }
-//
-//     // Types of waste recycled:
-//     waste_score += 24;
-//     waste_score -= 4 * list[5];
-//
-//     return waste_score;
-// }
-//
-// function get_waste(waste_score, max_waste) {
-//     return '%.2f'%(float(waste_score) / max_waste * 100);
-// }
-//
-// // People in household, house size, water (dishwasher/laundry)
-// function utility(list) {
-//     var utility_score = 0;
-//
-//     // People in household
-//     if (list[0] > 5) {
-//         utility_score += 2;
-//     }
-//     else if (list[0] == 5) {
-//         utility_score += 4;
-//     }
-//     else if (list[0] == 4) {
-//         utility_score += 6;
-//     }
-//     else if (list[0] == 3) {
-//         utility_score += 8;
-//     }
-//     else if (list[0] == 2) {
-//         utility_score += 10;
-//     }
-//     else if (list[0] == 1) {
-//         utility_score += 12;
-//     }
-//     else {
-//         utility_score += 14;
-//     }
-//
-//     // House size
-//     if (list[1] == 0) {
-//         utility_score += 10;
-//     }
-//     else if (list[1] == 1) {
-//         utility_score += 7;
-//     }
-//     else if (list[2] == 2) {
-//         utility_score += 4;
-//     }
-//     else {
-//         utility_score += 2;
-//     }
-//
-//     // Water usage per week
-//     if (list[2] > 18) {
-//         utility_score += 6;
-//     }
-//     else if (list[2] > 8) {
-//         utility_score += 4;
-//     }
-//     else if (list[2] >= 1) {
-//         utility_score += 1;
-//     }
-//
-//     return utility_score;
-// }
-//
-// function get_utility(utility_score, max_utility) {
-//     return '%.2f'%(float(utility_score) / max_utility * 100);
-// }
-//
-// function tier(dict) {
-//     var percent = get_transportation();
-//     dict = tier_res(dict, percent, "transportation");
-//     percent = self.get_waste();
-//     dict = tier_res(dict, percent, "waste");
-//     percent = self.get_utility();
-//     dict = tier_res(dict, percent, "utility");
-//     return dict;
-// }
-//
-// function tier_res(dict, percentage, category) {
-//     if (percent > 75) {
-//         dict[category] = ["poor"];
-//     }
-//     else if (percent > 50) {
-//         dict[category] = ["average"];
-//     }
-//     else if (percent > 25) {
-//         dict[category] = ["good"];
-//     }
-//     else if (percent >= 0) {
-//         dict[category] = ["excellent"];
-//     }
-//     return dict;
-// }
-
-
-// Completing a goal: subtract points?
-function goal_update(task) {
-    if (task.status) {
-        if (task.category.equals("Transportation")) {
-            transportation_score -= 4;
+    // Personal/public/flights and miles per year
+    car_transportation(car) {
+        // miles of car usage per year
+        if (car > 15000) {
+            this.transportation_score += 15;
         }
-        else if (task.category.equals("Utilities")) {
-            utility_score -= 2;
+        else if (car > 10000) {
+            this.transportation_score += 10;
         }
-        else if (task.category.equals("Consumption/Waste")) {
-            waste_score -= 8;
+        else if (car > 1000) {
+            this.transportation_score += 6;
+        }
+        else if (car > 0) {
+            this.transportation_score += 4;
+        }
+    }
+
+    public_transportation(buses) {
+        // miles of public transportation usage per year
+        if (buses > 20000) {
+            this.transportation_score += 20;
+        }
+        else if (buses > 15000) {
+            this.transportation_score += 10;
+        }
+        else if (buses > 10000) {
+            this.transportation_score += 6;
+        }
+        else if (buses > 1000) {
+            this.transportation_score += 4;
+        }
+        else if (buses > 0) {
+            this.transportation_score += 2;
+        }
+    }
+
+    flight_transportation(flight) {
+        // flight distance per year
+        if (flight == 0) {
+            this.transportation_score += 2;
+        }
+        else if (flight == 1) {
+            this.transportation_score += 6;
+        }
+        else if (flight == 2) {
+            this.transportation_score += 20;
+        }
+    }
+
+    get transportation() {
+        return '%.2f'%(this.transportation_score / this.max_transportation);
+    }
+
+    //Eating meat, household purchases, trash cans per week, types of waste recycled
+    calculate_waste(list) {    
+        // Meat per week
+        if (list[0] == 7) {
+            this.waste_score += 10;
+        }
+        else if (list[0] > 2) {
+            this.waste_score += 8;
+        }
+        else if (list[0] > 0) {
+            this.waste_score += 5;
+        }
+    
+        // Vegan y/n
+        if (list[1] == 1) {
+            this.waste_score += 2;
+        }
+        else if (list[1] == 0 && list[0] == 0) {
+            this.waste_score += 4;
+        }
+    
+        // Diet type (prepackaged, fresh, mix)
+        if (list[2] == 0) {
+            this.waste_score += 12;
+        }
+        else if (list[2] == 1) {
+            this.waste_score += 6;
+        }
+        else if (list[2] == 2) {
+            this.waste_score += 2;
+        }
+    
+        // Household purchases per year
+        if (list[3] > 7) {
+            this.waste_score += 10;
+        }
+        else if (list[3] > 5) {
+            this.waste_score += 8;
+        }
+        else if (list[3] > 3) {
+            this.waste_score += 6;
+        }
+        else if (list[3] > 0) {
+            this.waste_score += 4;
         }
         else {
-            transportation_score -= 1;
-            utility_score -= 1;
-            waste_score -= 1;
+            this.waste_score += 2;
+        }
+    
+        // Trash cans filled per week
+        if (list[4] >= 4) {
+            this.waste_score += 50;
+        }
+        else if (list[4] == 3) {
+            this.waste_score += 40;
+        }
+        else if (list[4] == 2) {
+            this.waste_score += 30;
+        }
+        else if (list[4] == 1) {
+            this.waste_score += 20;
+        }
+        else {
+            this.waste_score += 5;
+        }
+    
+        // Types of waste recycled:
+        this.waste_score += 24;
+        this.waste_score -= 4 * list[5];
+    }
+    
+    get waste() {
+        return '%.2f'%(float(this.waste_score) / this.max_waste * 100);
+    }
+    
+    // People in household, house size, water (dishwasher/laundry)
+    calculate_utility(list) {
+        // People in household
+        if (list[0] > 5) {
+            this.utility_score += 2;
+        }
+        else if (list[0] == 5) {
+            this.utility_score += 4;
+        }
+        else if (list[0] == 4) {
+            this.utility_score += 6;
+        }
+        else if (list[0] == 3) {
+            this.utility_score += 8;
+        }
+        else if (list[0] == 2) {
+            this.utility_score += 10;
+        }
+        else if (list[0] == 1) {
+            this.utility_score += 12;
+        }
+        else {
+            this.utility_score += 14;
+        }
+    
+        // House size
+        if (list[1] == 0) {
+            this.utility_score += 10;
+        }
+        else if (list[1] == 1) {
+            this.utility_score += 7;
+        }
+        else if (list[2] == 2) {
+            this.utility_score += 4;
+        }
+        else {
+            this.utility_score += 2;
+        }
+    
+        // Water usage per week
+        if (list[2] > 18) {
+            this.utility_score += 6;
+        }
+        else if (list[2] > 8) {
+            this.utility_score += 4;
+        }
+        else if (list[2] >= 1) {
+            this.utility_score += 1;
         }
     }
-    tier(tiers);
-    return [total_score, transportation_score, waste_score, utility_score];
+    
+    get utility() {
+        return '%.2f'%(float(this.utility_score) / this.max_utility * 100);
+    }
+
+    get total() {
+        return '%.2f'%(float(this.total_score) / this.max_total * 100);
+    }
+    
+    tier(dict) {
+        var percent = get_transportation();
+        dict = tier_res(dict, percent, "transportation");
+        percent = self.get_waste();
+        dict = tier_res(dict, percent, "waste");
+        percent = self.get_utility();
+        dict = tier_res(dict, percent, "utility");
+        return dict;
+    }
+    
+    tier_res(dict, percentage, category) {
+        if (percent > 75) {
+            dict[category] = ["poor"];
+        }
+        else if (percent > 50) {
+            dict[category] = ["average"];
+        }
+        else if (percent > 25) {
+            dict[category] = ["good"];
+        }
+        else if (percent >= 0) {
+            dict[category] = ["excellent"];
+        }
+        return dict;
+    }
+
+
+    // Completing a goal: subtract points?
+    goal_update(task) {
+        if (task.status) {
+            if (task.category.equals("Transportation")) {
+                this.transportation_score -= 4;
+            }
+            else if (task.category.equals("Utilities")) {
+                this.utility_score -= 2;
+            }
+            else if (task.category.equals("Consumption/Waste")) {
+                this.waste_score -= 8;
+            }
+            else {
+                this.transportation_score -= 1;
+                this.utility_score -= 1;
+                this.waste_score -= 1;
+            }
+            if (this.transportation_score < 0) {
+                this.transportation_score = 0;
+            }
+            if (this.utility_score < 0) {
+                this.utility_score = 0;
+            }
+            if (this.waste_score < 0) {
+                this.waste_score = 0;
+            }
+        }
+        tier(tiers);
+        return [total_score, this.transportation, this.waste, this.utility];
+    }
 }
